51c363936f1fc1f52242f3b1ca9e80d3cdd8b5c2
Kazumasa Wakamori <wakamori.kazumasa.17@shizuoka.ac.jp>
2020-03-22 16:12:55 +0900

    リファレンスの追加

--- src/SocialInsurance.py
+++ src/SocialInsurance.py
@@ -1,12 +1,13 @@
 class SocialInsurance:
     # 社会保険料
     @staticmethod
-    def calc(earnedIncome):
+    def calc(total_income):
         deduction = 0
-        # 健康保険 https://www.kyoukaikenpo.or.jp/~/media/Files/shared/hokenryouritu/r2/ippan/r2030113tokyo.pdf
-        deduction += earnedIncome * 0.0987 / 2
-        # 厚生年金
-        deduction += earnedIncome * 0.183 / 2
-        # 雇用保険
-        deduction += earnedIncome * 0.003
+        # 健康保険, 厚生年金 https://www.kyoukaikenpo.or.jp/~/media/Files/shared/hokenryouritu/r2/ippan/r2030113tokyo.pdf
+        deduction += total_income * 0.0987 / 2
+        deduction += total_income * 0.183 / 2
+
+        # 雇用保険 https://www.mhlw.go.jp/content/000484772.pdf
+        deduction += total_income * 0.003
+        
         return deduction
